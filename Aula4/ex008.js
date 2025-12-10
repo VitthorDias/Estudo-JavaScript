7286f754c805515c7485fb1763339aace4ff7cac
Vitthor Dias <v.fernandes@academico.ifg.edu.br>
2025-12-10 12:57:43 -0300

    Criação do modelo para serem utilizados nos exercicios

---
+++ Aula4/ex008.js
@@ -0,0 +1,9 @@
+var vel = 40.0
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 40){
+    console.log(`Acima da velocidade permitida!`)
+} else {
+    console.log(`Dentro da velocidade permitida!`)
+}
